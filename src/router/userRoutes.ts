771369dc3225d1ecc7a8213608dc2c5480f29ec4
MarcosAnteroDoCarmo <marcosantero@hotmail.com>
2022-06-19 22:36:31 -0300

    feat: add new pages

--- src/router/userRoutes.ts
+++ src/router/userRoutes.ts
@@ -5,37 +5,55 @@ const router = express.Router();
 
 router.post("/users", async (req, res) => {
   try {
+    const { name } = req.body;
     const { email } = req.body;
+    const { password } = req.body;
 
-    if (await User.findOne({ email })) {
-      throw { err: "User already exists" };
-    } else {
-      const user = await User.create(req.body);
+    if (!name) throw new Error("I need Name for this!");
 
-      user.password = undefined;
+    if (!email) throw new Error("I need Email for this!");
 
-      return res.send({ user, message: "New user created" });
-    }
-  } catch (message) {
-    return res.status(400).send({ message: "Error creating new user" });
+    if (!password) throw new Error("I need Password for this!");
+
+    if (await User.findOne({ email }))
+      throw new Error("This Email already exists");
+
+    const user = await User.create(req.body);
+
+    user.password = undefined;
+
+    return res.send({ user, message: "New user created" });
+  } catch (err: unknown) {
+    if (err instanceof Error) return res.status(400).send(err.message);
+    console.log(err);
+    return res.status(500).send("Server Error");
+  }
+});
+
+router.delete("/users/", async (req, res) => {
+  try {
+    throw new Error("I need Email for this!");
+  } catch (err: unknown) {
+    if (err instanceof Error) return res.status(400).send(err.message);
+    return res.status(500).send("Server Error");
   }
 });
 
 router.delete("/users/:email", async (req, res) => {
   try {
-    const { email } = req.params;
+    const email = req.params.email;
 
-    if (!email) {
-      throw { err: "I need email for this!" };
-    }
-    const user = await User.findOneAndDelete(req.params);
+    if (!email) throw new Error("I need email for this!");
 
-    user.password = undefined;
+    if (!(await User.findOne({ email })))
+      throw new Error("This email does not exist");
 
-    return res.send({ user, message: "User deleted" });
-  } catch (err) {
-    console.error(err);
-    return res.status(400).send({ err: "Error" });
+    await User.deleteOne({ email });
+
+    return res.send({ message: "User deleted" });
+  } catch (err: unknown) {
+    if (err instanceof Error) return res.status(400).send(err.message);
+    return res.status(500).send("Server Error");
   }
 });
 
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import router from "../router/userRoutes";
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
 const portaDoServidor = 3002;
+
+app.post("/", (req, res) => res.send(req.body)); 
 
 app.listen(portaDoServidor, () => {
   console.log(`O servidor esta rodando na porta ${portaDoServidor}`);
